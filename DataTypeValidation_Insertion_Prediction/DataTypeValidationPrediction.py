becc0f7239b074601d164a98adaf7f5fa3f673e6
Nusri7 <321nusri@gmail.com>
2023-08-11 06:45:17 +0530

    created DataTypeValidationPrediction.py

--- DataTypeValidation_Insertion_Prediction/DataTypeValidationPrediction.py
+++ DataTypeValidation_Insertion_Prediction/DataTypeValidationPrediction.py
@@ -11,9 +11,7 @@ class dBOperation:
     """
           This class shall be used for handling all the SQL operations.
 
-          Written By: iNeuron Intelligence
-          Version: 1.0
-          Revisions: None
+
 
           """
 
@@ -31,10 +29,6 @@ class dBOperation:
                         Description: This method creates the database with the given name and if Database already exists then opens the connection to the DB.
                         Output: Connection to the DB
                         On Failure: Raise ConnectionError
-
-                         Written By: iNeuron Intelligence
-                        Version: 1.0
-                        Revisions: None
 
                         """
         try:
@@ -57,10 +51,6 @@ class dBOperation:
            Description: This method creates a table in the given database which will be used to insert the Good data after raw data validation.
            Output: None
            On Failure: Raise Exception
-
-            Written By: iNeuron Intelligence
-           Version: 1.0
-           Revisions: None
 
         """
         try:
@@ -109,9 +99,6 @@ class dBOperation:
                                        Output: None
                                        On Failure: Raise Exception
 
-                                        Written By: iNeuron Intelligence
-                                       Version: 1.0
-                                       Revisions: None
 
                 """
 
@@ -158,10 +145,6 @@ class dBOperation:
                                                     above created .
                                        Output: None
                                        On Failure: Raise Exception
-
-                                        Written By: iNeuron Intelligence
-                                       Version: 1.0
-                                       Revisions: None
 
                 """
 
